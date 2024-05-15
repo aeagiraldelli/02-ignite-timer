8ff1eaaf23b8809e85ba2db6266b7859a21cea32
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2024-05-14 21:05:39 -0300

    chore: project structure.

---
+++ src/styles/global.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  *, *::after, *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${props => props.theme['green-500']}
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    background: ${props => props.theme['gray-900']};
+    color: ${props => props.theme['gray-300']}
+  }
+
+  a, button {
+    cursor: pointer;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1.6rem;
+  }
+`;
